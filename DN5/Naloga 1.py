44cda91b302ab438d7681e0f95bdd075c6df0cae
Rok <rp9376@student.uni-lj.si>
2022-12-04 19:29:17 +0100

    Dodajanje 5 DN

---
+++ DN5/Naloga 1.py
@@ -0,0 +1,22 @@
+import requests
+import random
+
+
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+]
+
+def get_html(url):
+  headers = {'User-Agent': user_agent_list[random.randint(0, len(user_agent_list)-1)]}
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    return(response.text)
+  else:
+    return False    
+
+page = get_html("https://example.com/")
+print(page)
